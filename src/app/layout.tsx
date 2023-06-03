a7f9585de4ea493597b2b678ce8b49c34319236b
vmt82 <valentin.tudor11@gmail.com>
2023-06-03 23:51:29 +0300

    add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers>
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkMondeSwitch from "./DarkMondeSwitch";
 
 export default function Header() {
   return (
@@ -11,9 +12,10 @@ export default function Header() {
         <MenuItem title="Home" address="/" Icon={AiFillHome} />
         <MenuItem title="About" address="/about" Icon={BsInfoCircleFill} />
       </div>
-      <div className="">
+      <div className="flex items-center space-x-5">
+        <DarkMondeSwitch/>
         <Link href="/" className="">
-          <span className="text-2x font-bold bg-amber-500 rounded-lg py-1 px-2 mr-1">
+          <span className="text-2xl font-bold bg-amber-500 rounded-lg py-1 px-2 mr-1">
             IMDb
           </span>
           <span className="text-xl font-medium hidden sm:inline">Clone</span>
